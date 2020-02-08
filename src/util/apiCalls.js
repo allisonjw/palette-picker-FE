6446bc8fac07bd4ecc477b9dcc5538a73079117c
Allison Wagner <allisonjw01@gmail.com>
2020-02-08 16:53:38 -0700

    Adding base url and start of async method to fetch all projects

---
+++ src/util/apiCalls.js
@@ -0,0 +1,12 @@
+const baseUrl = 'https://palette-of-colors-picker.herokuapp.com/api/v1';
+
+export const getProjects = async () => {
+    const response = await fetch(`${baseUrl}/projects`)
+    if (!response.ok) {
+      throw Error('Unable to get projects. Try again later.')
+    }
+    const projects = await response.json();
+    console.log(projects)
+    return projects;
+};
+  
