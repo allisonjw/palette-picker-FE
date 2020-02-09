dd5bb78a2dc61863e6908f8380432386e1d6a37c
Allison Wagner <allisonjw01@gmail.com>
2020-02-09 12:05:45 -0700

    Adds useEffect to get palette data

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,20 @@
+
 import React, { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 import { getAllProjects, getAllPalettes } from '../util/apiCalls';
 import './App.scss';
 
 const App = () => {
+  // const dispatch = useDispatch();
   const [projects, setProjects] = useState([]);
+  const [palettes, setPalettes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
         const response = await getAllProjects();
-        console.log('response', response)
+        console.log('responseProjects', response)
         setProjects(response.projects);
      // dispatch(setAllProjects(response));
       } catch(error) {
@@ -20,9 +24,25 @@ const App = () => {
     fetchProjects();
   }, []);
 
-  if (loading) {
-    return <h2>Loading...</h2>
-  };
+
+  useEffect(() => {
+    const fetchPalettes = async () => {
+      try {
+        const response = await getAllPalettes();
+        console.log('responsePalettes', response)
+        setPalettes(response.palettes);
+     // dispatch(setAllPalettes(response));
+      } catch(error) {
+          console.log(error);
+      }
+    };
+    fetchPalettes();
+  }, []);
+
+  // if (loading) {
+  //   return <h2>Loading...</h2>
+  // };
+
   return (
     <div className="App">
       <header className="App-header">
