1eed7b8205627c8ea001917fe24b2bca2c3f0025
Jessie Le-Ho <lehoj@oregonstate.edu>
2020-02-08 18:44:19 -0700

    Set up Project component

---
+++ src/Project/Project.js
@@ -0,0 +1,14 @@
+//import my project form
+//import color card???
+import React from 'react';
+import './Project.scss'
+
+const Project = () => {
+    return(
+        <div>
+            //whatever component goes in here
+        </div>
+    )
+}
+
+export default Project;
