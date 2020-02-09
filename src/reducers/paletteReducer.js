d6a67089f7c0d0fda20f1f58652090782feffade
Jessie Le-Ho <lehoj@oregonstate.edu>
2020-02-09 14:56:44 -0700

    Set up palette reducer

---
+++ src/reducers/paletteReducer.js
@@ -0,0 +1,15 @@
+export const paletteReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_PALETTE':
+            console.log('action palettes', action.palettes)
+            return action.palettes
+        case 'ADD_PALETTE':
+            return [...state, action.palette]
+        case 'DELETE_PALETTE':
+            return [...state].filter(palette => palette.id !== action.id)
+        case 'LOCK_COLOR':
+            return action.isLocked
+        default : 
+            return state
+    }
+}
