4363f262b67e06ee74ca782bc211f52926bbef48
Jessie Le-Ho <lehoj@oregonstate.edu>
2020-02-08 18:32:29 -0700

    Set up Project Form

---
+++ src/ProjectForm/ProjectForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './ProjectForm.scss';
+
+class ProjectForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <form>
+                <input/>
+                <button>Submit Project</button>
+            </form>
+        )
+    }
+}
+
+export default ProjectForm;
