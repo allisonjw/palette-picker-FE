ab128d9df9118c850caa2815bc79f9fe8e39257c
Allison Wagner <allisonjw01@gmail.com>
2020-02-09 11:04:16 -0700

    Adds useEffect and useState to app for projects

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllProjects, getAllPalettes } from '../util/apiCalls';
 import './App.scss';
 
 const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await getAllProjects();
+        console.log('response', response)
+        setProjects(response.projects);
+     // dispatch(setAllProjects(response));
+      } catch(error) {
+          console.log(error);
+      }
+    };
+    fetchProjects();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  };
   return (
     <div className="App">
       <header className="App-header">
