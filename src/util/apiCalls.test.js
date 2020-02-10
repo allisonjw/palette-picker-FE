7b5f91876a0516ef6c9d0d94eef4d2d800b9fb16
Allison Wagner <allisonjw01@gmail.com>
2020-02-10 12:04:33 -0700

    Adds testing for getAllPalettes

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -47,8 +47,59 @@ describe('apiCalls.js', () => {
     });
 
     describe('getAllPalettes', () => {
-
-    });
+        let mockResponse = {
+            "results": [
+                {
+                    id: 9,
+                    palette_name: "Neutrals",
+                    project_id: 10,
+                    color_1: "#D6B28D",
+                    color_2: "#C1A281",
+                    color_3: "#E5CCA9",
+                    color_4: "#BFA98B",
+                    color_5: "#A88F77",
+                    timestamps: null
+                }
+            ]
+          }
+      
+          beforeEach(() => {
+            window.fetch = jest.fn().mockImplementation(() => {
+              return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(mockResponse)
+              });
+            });
+          });
+      
+          it('should fetch with correct url', () => {
+            const mockUrl = 'https://palette-of-colors-picker.herokuapp.com/api/v1/palettes'
+      
+            getAllPalettes(mockUrl)
+      
+            expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+          })
+      
+          it('should return an array of palettes (HAPPY)', () => {
+            const mockUrl = 'https://palette-of-colors-picker.herokuapp.com/api/v1/palettes'
+      
+            getAllPalettes(mockUrl)
+            .then(results => expect(results).toEqual(mockResponse))
+          })
+      
+          it('should return an error (SAD)', () => {
+            window.fetch = jest.fn().mockImplementation(() => {
+              return Promise.resolve({
+                ok: false,
+                statusText: "Unable to get palettes. Try again later."
+              })
+            })
+      
+            const mockUrl = 'http://palette-pick-be.herokuapp.com/api/v1/palette'
+      
+            expect(getAllPalettes(mockUrl)).rejects.toEqual(Error("Unable to get palettes. Try again later."))
+          })
+    })
 
     describe('getProjectById', () => {
 
