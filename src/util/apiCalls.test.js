5ec02d8dadbce0b214f43a5164f74e3bf257b62a
Allison Wagner <allisonjw01@gmail.com>
2020-02-10 11:56:07 -0700

    Adds test with mockdata for get all projects

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -3,7 +3,47 @@ import { getAllProjects, getProjectById, getPaletteById, getAllPalettes, addPale
 describe('apiCalls.js', () => {
 
     describe('getAllProjects', () => {
-
+        let mockResponse = {
+            "results": [
+                {
+                    id: 9,
+                    project_name: "Jessie's House",
+                    user_id: 5
+                },
+                {
+                    id: 10,
+                    project_name: "Dream Bath",
+                    user_id: 5
+                },
+                {
+                    id: 11,
+                    project_name: "Allison's House",
+                    user_id: 6
+                },
+                {
+                    id: 12,
+                    project_name: "Art Project",
+                    user_id: 6
+                }
+            ]
+          }
+      
+          beforeEach(() => {
+            window.fetch = jest.fn().mockImplementation(() => {
+              return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(mockResponse)
+              });
+            });
+          });
+      
+          it('should fetch with correct url', () => {
+            const mockUrl = 'https://palette-of-colors-picker.herokuapp.com/api/v1/projects'
+      
+            getAllProjects(mockUrl)
+      
+            expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+          })
     });
 
     describe('getAllPalettes', () => {
