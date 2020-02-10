fd708d3cab4a8e7f33951cfa7a407fc6a81da59d
Allison Wagner <allisonjw01@gmail.com>
2020-02-09 20:27:32 -0700

    Passes props down to project to display name

--- src/ProjectContainer/ProjectContainer.js
+++ src/ProjectContainer/ProjectContainer.js
@@ -1,7 +1,7 @@
 import Project from '../Project/Project';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setProjects } from '../actions';
+import { setProjects, deleteProject } from '../actions';
 import { getAllProjects } from '../util/apiCalls';
 import './ProjectContainer.scss';
 
