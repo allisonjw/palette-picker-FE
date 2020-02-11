1c2c2ae5783cc960c908a4822bed7ba2d9895742
Jessie Le-Ho <lehoj@oregonstate.edu>
2020-02-10 20:09:31 -0700

    Make palette form in hooks

--- src/PaletteForm/PaletteForm.js
+++ src/PaletteForm/PaletteForm.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import usePaletteForm from '../CustomHooks/CustomHooks.js';
 import './PaletteForm.scss';
 
-class PaletteForm extends Component {
-    constructor() {
-        super()
-        this.state = {
 
-        }
-    }
-    render() {
-        return(
-            <form className="palette_form">
-                <select className="palette_select">project # 1</select>
-                <input className="palette_input"/>
-                <button className="palette_btn">Submit Palette</button>
-            </form>
-        )
-    }
+
+const PaletteForm = () => {
+    const {input, handleChange, handleSubmit} = usePaletteForm();
+    return(
+        <form className="palette_form" onSubmit={handleSubmit}> 
+            <select className="palette_select">project # 1</select>
+            <input className="palette_input" onChange={handleChange} value={input}/>
+            <button className="palette_btn">Submit Palette</button>
+        </form>
+    )
 }
 
 export default PaletteForm;
