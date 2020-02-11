d45d32f644a4f7f57a08dcd5cc4b94a7011183e0
Allison Wagner <allisonjw01@gmail.com>
2020-02-10 19:48:01 -0700

    Changing props to reflect what is being passed in

--- src/ColorCard/ColorCard.js
+++ src/ColorCard/ColorCard.js
@@ -3,16 +3,16 @@ import './ColorCard.scss';
 import openPadlock from '../images/open-lock.png';
 import closedPadlock from '../images/closed-lock.png'
 
-const ColorCard = ({ toggleLock, currentPalette}) => {
+const ColorCard = ({ color, lockPaletteColor}) => {
     // const lock = isLocked ? closedPadlock : openPadlock;
     return(
-        <section className="section_colorCards" style={{backgroundColor: `#${currentPalette}`}}>
+        <section className="section_colorCards" style={{backgroundColor: `#${color}`}}>
               <img className="colorCard_image-lock" 
             //   src={lock}
               src={require("../images/open-lock.png")}
               alt=""
-              onClick={() => toggleLock()}/>
-              <h3 className="colorCard_hex">#{currentPalette}</h3>
+              onClick={() => lockPaletteColor(color.palette_id)}/>
+              <h3 className="colorCard_hex">#{color}</h3>
         </section>
     )
 }
