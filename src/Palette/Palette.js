9f53f18243f3d13d7db5560e5f6504dbf27ae6cc
Allison Wagner <allisonjw01@gmail.com>
2020-02-12 15:03:01 -0700

    Fetches palettes saves to state and passes down to projects to display in project container

--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -2,32 +2,29 @@ import React from 'react';
 import './Palette.scss';
 import trashBin from '../images/rubbish.png'
 
-const Palette = () => {
+const Palette = ({ palette_name, color_1, color_2, color_3, color_4, color_5 }) => {
 
-    // pass all the props down from projects. dipslay the {palette_name}
-    // create an array const colors =[color_1, color_2,c color_3, color_4, color_5]
-    // map over colors and in a variable called {displayColors} and add inline styling
-    // like we did in ColorCard (style={{ backgroundColor: `#${color}` }}
+    const colors =[color_1, color_2, color_3, color_4, color_5];
 
-    // const displayColors = colors.map(color => {
-    //     return (
-    //       <div
-    //         className="palette__colors"
-    //         style={{ backgroundColor: `#${color}` }}
-    //       ></div>
-    //     );
-    // });
+    const displayColors = colors.map(color => {
+        return (
+          <div
+            className="palette__colors"
+            style={{ backgroundColor: `#${color}` }}
+          ></div>
+        );
+    });
 
     return (
         <section className="section_palettes">
             <h3 className="palette_name">{}</h3>
+                {displayColors}
             <img
                 className="palette_delete-btn"
                 src={trashBin}
                 alt="delete button"
                 // onClick={() => removePalette(palette_id)}
             />
-            {/* {displayColors} */}
         </section>
     )
 }
--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,28 +1,27 @@
-//import color card???
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Project.scss';
 import trashBin from '../images/rubbish.png';
 import Palette from '../Palette/Palette';
 
 const Project = ({projects}) => {
+   const palettes = useSelector(state => state.palettes)
+
+    const displayPalettes = palettes
+      ? 
+      palettes.map(palette => (
+      <Palette {...palette} key={palette.id} />))
+      : '';
+
     const displayNames = projects.map(project => {
-        return project.project_name
-    })
-
-    // We might need to fetch the palettes in project container and pass them down to project so we can display them?
-
-    //we either need to have delete methods in project or project container and pass them as props
-
-    // const displayPalettes = palettes
-    // ? palettes.map(palette => (
-    //     <Palette {...palette} key={palette.id} />
-    //   ))
-    // : '';
+      return project.project_name
+    });
     
     return(
         <div>
         <section className="section_projects">
             <h3 className="project_name">{displayNames}</h3>
+        {displayPalettes}
         </section>
           <img
             className='deleteBtn'
@@ -30,11 +29,8 @@ const Project = ({projects}) => {
             alt='delete button'
             // onClick={() => removeProject(project_id)}
           />
-        {/* {displayPalettes} */}
-        <Palette />
-        {/* once we get a method working to pass what needs to be displayed form palettes, we can remove <Pallete /> and just use {displayPalttes} */}
         </div>
     )
-}
+};
 
 export default Project;
