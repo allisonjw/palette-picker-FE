25c48c63cf2de853e1ac3ec82e0bb12456406ef5
Allison Wagner <allisonjw01@gmail.com>
2020-02-12 10:33:00 -0700

    Adds more classnames with styling and pseudo coding

--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -3,13 +3,28 @@ import './Palette.scss';
 import trashBin from '../images/rubbish.png'
 
 const Palette = () => {
+
+    // pass all the props down from projects. dipslay the {palette_name}
+    // create an array const colors =[color_1, color_2,c color_3, color_4, color_5]
+    // map over colors and in a variable called {displayColors} and add inline styling
+    // like we did in ColorCard (style={{ backgroundColor: `#${color}` }}
+
+    // const displayColors = colors.map(color => {
+    //     return (
+    //       <div
+    //         className="palette__colors"
+    //         style={{ backgroundColor: `#${color}` }}
+    //       ></div>
+    //     );
+    // });
+
     return (
         <section className="section_palettes">
             <h3 className="palette_name">{}</h3>
             <img
-                className='deleteBtn'
+                className="palette_delete-btn"
                 src={trashBin}
-                alt='delete button'
+                alt="delete button"
                 // onClick={() => removePalette(palette_id)}
             />
             {/* {displayColors} */}
