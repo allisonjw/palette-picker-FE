c9f00f330ffb09038dba8e88075bad72a31eba28
Allison Wagner <allisonjw01@gmail.com>
2020-02-11 17:26:52 -0700

    Adds functionality to get random color to generate

--- src/ColorCard/ColorCard.js
+++ src/ColorCard/ColorCard.js
@@ -3,19 +3,22 @@ import './ColorCard.scss';
 import openPadlock from '../images/open-lock.png';
 import closedPadlock from '../images/closed-lock.png'
 
-const ColorCard = ({ color, locked, palette, lockPaletteColor}) => {
+const ColorCard = ({ id, color, locked, palette_id, lockColor}) => {
   // const [isLocked, setIsLocked] = useState()
   console.log('color', color)
-  console.log('locked', locked)
-  console.log('palette', palette)
+  // console.log('locked', locked)
+  // console.log('initialPalette', initialPalette)
+  // console.log(lockColor)
+  // console.log(id)
 
-    const lock = locked ? openPadlock : closedPadlock;
+    const toggleLock = !locked ? openPadlock : closedPadlock;
     return(
         <section className="section_colorCards" style={{backgroundColor: `#${color}`}}>
               <img className="colorCard_image-lock" 
-              src={lock}
+              src={toggleLock}
+              // src={require('../images/open-lock.png')}
               alt="lock"
-              onClick={(e) => lockPaletteColor(e, palette.palette_id)}/>
+              onClick={() => lockColor(id)} />
               <h3 className="colorCard_hex">#{color}</h3>
         </section>
     )
--- src/ColorContainer/ColorContainer.js
+++ src/ColorContainer/ColorContainer.js
@@ -1,66 +1,94 @@
 import ColorCard from '../ColorCard/ColorCard';
-import { addPalette, deletePalette } from '../util/apiCalls';
-import { deleteColorPalette } from '../actions';
-import { useDispatch } from 'react-redux';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './ColorContainer.scss';
 
 const ColorContainer = () => {
-    const dispatch = useDispatch()
-    const [palette, setPalette] = useState([
-        {palette_id: 1, color: '', locked: false},
-        {palette_id: 2, color: '', locked: false},
-        {palette_id: 3, color: '', locked: false},
-        {palette_id: 4, color: '', locked: false},
-        {palette_id: 5, color: '', locked: false}
-      ]) 
+    const [initialPalette, setInitialPalette] = useState([
+        {palette_id: 1, color: '#CA58AD', locked: false},
+        {palette_id: 2, color: '#40F8A5', locked: false},
+        {palette_id: 3, color: '#7D06F0', locked: false},
+        {palette_id: 4, color: '#A545F5', locked: false},
+        {palette_id: 5, color: '#341ADB', locked: false}
+    ]);     
 
-    const generateRandomColor = () => {
-        setPalette([
-            {palette_id: 1, color: Math.floor(Math.random()*16777215).toString(16), locked: false},
-            {palette_id: 2, color: Math.floor(Math.random()*16777215).toString(16), locked: false},
-            {palette_id: 3, color: Math.floor(Math.random()*16777215).toString(16), locked: false},
-            {palette_id: 4, color: Math.floor(Math.random()*16777215).toString(16), locked: false},
-            {palette_id: 5, color: Math.floor(Math.random()*16777215).toString(16), locked: false}
-        ]);
-      };
+      const changeColor = () => {
+        const randomColor = initialPalette.map(initPalette => {
+            if (initPalette.locked === false) {
+                initPalette.color = `${Math.floor(Math.random()*16777215).toString(16)}`;
+            }
+            return initPalette
+        })
+        setInitialPalette(randomColor)
+      }
 
-      const lockPaletteColor = (e, palette_id) => {
-        e.preventDefault();
-        setPalette([
-          ...palette, {
-            palette_id: palette_id, color: palette.color, locked: !palette.locked
-          }
-        ])
-      };
-    
-      useEffect(() => {
-        generateRandomColor();
-      }, []);
-    
+      const lockColor = (id) => {
+          const updatedColors = initialPalette.map(color => {
+            if (color.palette_id === id) {
+              color.locked = !color.locked
+            }
+            return color;
+          });
+          console.log('lockcolor:', updatedColors)
+          setInitialPalette(updatedColors)
+      }
+      
+    //   const generateRandomColor = () => {
+    //       return Math.floor(Math.random()*16777215).toString(16);
+    //   }
 
-    // const toggleLock = (isLocked) => {
-    //     dispatch(lockColor(false))
-    // }
+      const displayColorCard = initialPalette.map(palette => {
+        return (
+            <ColorCard 
+              {...palette}
+              key={palette.palette_id}
+              lockColor={lockColor}
+            //   changeColor={changeColor}
+            />
+        )
+      })
 
-    const deleteColorPalette = (id) => {
-        dispatch(deleteColorPalette(id))
-    }
-
+      
     return(
         <>
         <section className="section_colorContainer">
-            <ColorCard color={palette[0].color} palette={palette} locked={palette[0].locked} lockPaletteColor={lockPaletteColor}/>
-            <ColorCard color={palette[1].color} palette={palette} locked={palette[1].locked} lockPaletteColor={lockPaletteColor}/>
-            <ColorCard color={palette[2].color} palette={palette} locked={palette[2].locked} lockPaletteColor={lockPaletteColor}/>
-            <ColorCard color={palette[3].color} palette={palette} locked={palette[3].locked} lockPaletteColor={lockPaletteColor}/>
-            <ColorCard color={palette[4].color} palette={palette} locked={palette[4].locked} lockPaletteColor={lockPaletteColor}/>
+            {displayColorCard}
+            {/* <ColorCard 
+                // onClick={reply_click(id)}
+                color={initialPalette[0].color} 
+                initialPalette={initialPalette} 
+                locked={initialPalette[0].locked} 
+                id={initialPalette[0].palette_id}
+                lockColor={lockColor}/>
+            <ColorCard 
+                color={initialPalette[1].color} 
+                initialPalette={initialPalette} 
+                locked={initialPalette[1].locked}
+                id={initialPalette[1].palette_id} 
+                lockColor={lockColor}/>
+            <ColorCard 
+                color={initialPalette[2].color} 
+                initialPalette={initialPalette} 
+                locked={initialPalette[2].locked} 
+                id={initialPalette[2].palette_id}
+                lockColor={lockColor}/>
+            <ColorCard 
+                color={initialPalette[3].color} 
+                initialPalette={initialPalette} 
+                locked={initialPalette[3].locked} 
+                id={initialPalette[3].palette_id}
+                lockColor={lockColor}/>
+            <ColorCard 
+                color={initialPalette[4].color} 
+                initialPalette={initialPalette} 
+                locked={initialPalette[4].locked}
+                id={initialPalette[4].palette_id} 
+                lockColor={lockColor}/> */}
         </section>
         <section className="generate_btn-section">   
         <button
             type='button'
             className='generate_colors-btn'
-            onClick={generateRandomColor}>GENERATE COLORS
+            onClick={() => changeColor()}>GENERATE COLORS
         </button>
         </section>
         </>
